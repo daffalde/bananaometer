74e07b7ae10cd0aac0f292fdd5f88ec99a209e5c
daffalde <daffalderayhan@gmail.com>
2026-01-11 09:37:57 +0700

    Refactor code structure for improved readability and maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,30 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className="container">
-      <h1>halo</h1>
+      <div className="hero-bg" id="hero">
+        <div className="hero-text">
+          <div>
+            <h1>banana</h1>
+          </div>
+          <div>
+            <h1>o</h1>
+          </div>
+          <div>
+            <h1>meter</h1>
+          </div>
+        </div>
+        <img
+          className="hero-bg-banana"
+          src="/hero-bg1.png"
+          alt="banana image for hero section"
+        />
+        <img
+          className="hero-bg-banana"
+          src="/hero-bg2.png"
+          alt="banana image for hero section"
+          style={{ zIndex: "3" }}
+        />
+      </div>
     </div>
   );
 }
